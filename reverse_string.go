5d5af1e081508e2be3939690913cb5a718bc3396
Edvard <vovagavr93@gmail.com>
2022-06-01 21:38:39 +0300

    completely correct works,i think

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,11 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
+	rune_slice := []rune(input)
+	var rune_final []rune
+	for i := len(rune_slice) - 1; i >= 0; i-- {
+		rune_final = append(rune_final, rune_slice[i])
+	}
+	output = string(rune_final)
 	return output
 }
